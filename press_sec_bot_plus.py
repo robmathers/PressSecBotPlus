c02f8c9d591ca70ba30a050c4f0a700bbf0354d3
valerybirz002 <valerybriz@gmail.com>
2017-11-12 06:17:41 -0700

    adding support to multiple accounts followed

--- press_sec_bot_plus.py
+++ press_sec_bot_plus.py
@@ -31,11 +31,11 @@ def save_config(config):
         config.write(f)
 
 
-def save_last_tweet(tweet_id):
+def save_last_tweet(tweet_id,account_to_follow):
     config = load_config()
     if not config.has_section('saved_state'):
         config.add_section('saved_state')
-    config.set('saved_state', 'last_tweet_id', str(tweet_id))
+    config.set('saved_state', 'last_tweet_id'+account_to_follow, str(tweet_id))
     save_config(config)
 
 
@@ -50,11 +50,12 @@ def api_from_config(config):
     return api
 
 
-def render_tweet_html(tweet):
-    date_format = '%B %-d, %Y'
+def render_tweet_html(tweet,account_to_follow):
+    date_format = '%d/%m/%Y'
     context = {
         'body': process_tweet_text(tweet),
-        'date': date.fromtimestamp(tweet.created_at_in_seconds).strftime(date_format)
+        'date': date.fromtimestamp(tweet.created_at_in_seconds).strftime(date_format),
+        'account': account_to_follow
     }
     return jinja2.Environment(
         loader=jinja2.FileSystemLoader('./')
@@ -130,13 +131,13 @@ def get_status_message():
 
     return message
 
-def release_tweet(tweet, api):
+def release_tweet(tweet,account_to_follow, api):
     """Formats and publishes a Tweet to the account"""
 
     if tweet.truncated:
         tweet = convert_truncated(tweet)
 
-    tweet_html = render_tweet_html(tweet)
+    tweet_html = render_tweet_html(tweet,account_to_follow)
     image = html_to_png(tweet_html)
 
     status = get_status_message()
@@ -187,7 +188,7 @@ def poll_for_updates(api, account_to_follow, starting_id=None, interval=300):
     else:
         latest_tweet_id = api.GetUserTimeline(screen_name=account_to_follow, count=1)[0].id
     while True:
-        new_tweets = api.GetUserTimeline(screen_name=account_to_follow, since_id=latest_tweet_id, count=200, include_rts=False)
+        new_tweets = api.GetUserTimeline(screen_name=account_to_follow, since_id=latest_tweet_id, count=10, include_rts=False)
 
         # process the list in reverse order, to preserve time-order
         for tweet in new_tweets[::-1]:
@@ -195,7 +196,7 @@ def poll_for_updates(api, account_to_follow, starting_id=None, interval=300):
 
         if len(new_tweets) > 0:
             latest_tweet_id = new_tweets[0].id
-            save_last_tweet(latest_tweet_id)
+            save_last_tweet(latest_tweet_id,account_to_follow)
 
         sleep(interval)
 
@@ -204,18 +205,18 @@ def release_backlog_tweets(api, account_to_follow, starting_id):
     """Release all tweets after a given tweet id, up to a maximum of 200. Repeats after each batch until no more updates are available."""
     backlog = api.GetUserTimeline(screen_name=account_to_follow,
                                   since_id=starting_id,
-                                  count=200,
+                                  count=10,
                                   include_rts=False)
     while len(backlog) > 0:
         print 'Got %d tweets starting at %d' % (len(backlog), backlog[0].id)
         # process the list in reverse order, to preserve time-order
         for tweet in backlog[::-1]:
-            release_tweet(tweet, api)
-            save_last_tweet(tweet.id)
+            release_tweet(tweet, account_to_follow, api)
+            save_last_tweet(tweet.id,account_to_follow)
 
         backlog = api.GetUserTimeline(screen_name=account_to_follow,
                                       since_id=tweet.id,
-                                      count=200,
+                                      count=10,
                                       include_rts=False)
 
 
@@ -242,8 +243,8 @@ def update_from_stream(api, account_to_follow, include_rts=False):
             message.user.screen_name == normalized_account and
             (not message.retweeted_status or include_rts)):
 
-                release_tweet(message, api)
-                save_last_tweet(message.id)
+                release_tweet(message,account_to_follow, api)
+                save_last_tweet(message.id, account_to_follow)
 
 
 def within_exception_rate_limit(exception_datestamps):
@@ -271,26 +272,32 @@ def within_exception_rate_limit(exception_datestamps):
 def main():
     config = load_config()
     api = api_from_config(config)
-
+    last_tweet_id ={}
     # Check that config file is writeable
     if not os.access(config_file, os.W_OK):
         print 'Write access to config file unavailable, exiting.'
         sys.exit(1)
 
-    account_to_follow = config.get('settings', 'account_to_follow')
+    accounts_to_follow = config.get('settings', 'account_to_follow')
 
-    try:
-        last_tweet_id = int(config.get('saved_state', 'last_tweet_id'))
-    except (NoOptionError, NoSectionError) as e:
-        last_tweet_id = None
+    for account_to_follow in accounts_to_follow.split(","):
+        try:
+            last_tweet_id[account_to_follow] = int(config.get('saved_state', 'last_tweet_id'+account_to_follow))
+            print account_to_follow
+            release_backlog_tweets(api, account_to_follow, last_tweet_id[account_to_follow])
+        except (NoOptionError, NoSectionError) as e:
+            last_tweet_id = None
 
-    release_backlog_tweets(api, account_to_follow, last_tweet_id)
+
 
     # Catch Twitter streaming errors, ratelimited to prevent infinite respawn
     exception_datestamps = []
-    while within_exception_rate_limit(exception_datestamps):
+    #while within_exception_rate_limit(exception_datestamps):
+    if(True):
         try:
-            update_from_stream(api, account_to_follow)
+            for account_to_follow in accounts_to_follow.split(","):
+                update_from_stream(api, account_to_follow)
+
         except (TwitterError, ChunkedEncodingError):
             exception_datestamps.append(datetime.now())
         except StopIteration:
